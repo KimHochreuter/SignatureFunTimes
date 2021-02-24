f2aae33ca93848a7e892166edba0269de38df165
Ida Burchardt Egendal <ida@virkplan.dk>
2021-02-24 10:20:56 +0100

    que

--- SparseSignaturesKimandIda.R
+++ SparseSignaturesKimandIda.R
@@ -83,7 +83,6 @@ for (k in 1:length(K_range)){
       M_CV <- M
       CV_idx <- as.matrix(expand.grid(1:G, 1:m)[sample(1:(G*m), ceiling(G*m*0.1),replace=FALSE),])
       M_CV[CV_idx] <- 0 #Replace approximately 1% of cells in matrix by 0.
-      MSE_CV <- 0
       for (s in 1:5){ #As suggested by paper, step 5c is repeated 5 times.
         for (l in 1:20){ #Repeated estimation to ensure convergence.
           for (j in 1:m){ #beta can only be updated col-wise, due to the function nnlasso
@@ -139,7 +138,7 @@ for (l in 1:20){
                                               lambda = lambda_best, 
                                               intercept = FALSE, 
                                               normalize = FALSE, 
-                                              tol = 1e-05, 
+                                              tol =  1e-5, 
                                               maxiter = 10000, 
                                               path = FALSE)$coef[2,])
   }
@@ -147,7 +146,7 @@ for (l in 1:20){
     alpha[i,] <- nnls(t(as.matrix(beta)),as.vector(M[i,]))$x
   }
 }
-par(mfrow=c(2,2))
+par(mfrow=c(3,2))
 for(i in 1:(K_best+1)){
   barplot(beta[i,])
 }
