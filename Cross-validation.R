fda9aa90e6839f443d895d0f4d3998d1a89e609b
Kim Hochreuter <kho@obh-gruppen.dk>
2021-03-03 12:54:55 +0100

    Now with MSE for every step

--- Cross-validation.R
+++ Cross-validation.R
@@ -1,36 +1,53 @@
-MSE <- c(0, 0, 10^7) #find better solution
-for (k in 1:length(K_range)){
-  for (lambda in lambda_range) {
-    MSE_CV <- 0
+library(NMF)
+
+load("patients.rda")
+M <- patients
+M <- M[rowSums(M)>1000,] #Removing patients with less than 1000 mutations
+G <- nrow(M) #Number of patients
+m <- ncol(M) #Number of mutation types
+
+K_range <- 2:5 #Number of signatures to investigate
+
+MSE <- list(0) #find better solution
+MSE_CV <- matrix(0, ncol = 10, nrow = 5)
+for (k in 2:max(K_range)){
+    #MSE_CV <- 0
     for (i in 1:10){
       M_CV <- M
-      CV_idx <- as.matrix(expand.grid(1:G, 1:m)[sample(1:(G*m), ceiling(G*m*0.1),replace=FALSE),])
+      CV_idx <- as.matrix(expand.grid(1:G, 1:m)[sample(1:(G*m), ceiling(G*m*0.01),replace=FALSE),])
       M_CV[CV_idx] <- 0 #Replace approximately 1% of cells in matrix by 0.
-      MSE_CV <- 0
-      for (s in 1:5){ #As suggested by paper, step 5c is repeated 5 times.
-        for (l in 1:20){ #Repeated estimation to ensure convergence.
-          for (j in 1:m){ #beta can only be updated col-wise, due to the function nnlasso
-            target <- M_CV[,j] - alpha[[k]][,1]*background[j] # vi får negative target values?
-            #target[target<0] <- 0
-            beta[[k]][2:(k+1),j] <- as.vector(nnlasso(x = alpha[[k]][,2:(k+1), drop=FALSE], 
-                                                      y = target, 
-                                                      family = "normal", 
-                                                      lambda = lambda, 
-                                                      intercept = FALSE, 
-                                                      normalize = FALSE, 
-                                                      tol = 1e-05, 
-                                                      maxiter = 10000, 
-                                                      path = FALSE)$coef[2,])
-          }
-          for(i in 1:G){ #update alphas
-            alpha[[k]][i,] <- nnls(t(as.matrix(beta[[k]])),as.matrix(M_CV[i,]))$x
-          }
-        }
+      for (s in 1:5){
+        
+        NMF <- nmf(M_CV, rank = k, nrun = 10)
+        beta = t(basis(NMF))
+        alpha = t(coef(NMF))
         #we replace the zero cells with the estimate from the CV. This is run 5 times, with updated M
-        M_CV[CV_idx] <- (alpha[[k]]%*%beta[[k]])[CV_idx]
+        M_CV[CV_idx] <- (t(alpha%*%beta))[CV_idx]
+        MSE_CV[s,i] <- mean((as.vector(M[CV_idx,])-(t(alpha%*%beta))[CV_idx])^2)
+        print(MSE_CV)
       }
-      MSE_CV <- MSE_CV +  mean((as.vector(M[CV_idx,])-(alpha[[k]]%*%beta[[k]])[CV_idx,])^2)
     }
-    MSE <- rbind(MSE, c(k, lambda, MSE_CV))
-  }
+  MSE[[k]] = MSE_CV
+  print(MSE)
+  message(100*k/(max(K_range)), "%")
 }
+
+'
+
+2+2
+NMF <- nmf(M, rank = 6, nrun = 10)
+beta = t(basis(NMF))
+alpha = t(coef(NMF))
+
+alpha
+
+par(mfrow=c(3,2))
+for(i in 1:(6)){
+  barplot(alpha[,i])
+}
+
+asd
+dsa = paste(k,"tisser")
+asd$"2 tisser" = c(seq(1,10))
+asd
+'
