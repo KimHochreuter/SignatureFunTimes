6792fdd113b2bc71473a47be8637634e5a57a68a
Ida Burchardt Egendal <ida@virkplan.dk>
2021-03-04 09:34:54 +0100

    Plot the results

--- Cross-validation.R
+++ Cross-validation.R
@@ -18,16 +18,19 @@ for (k in 2:max(K_range)){
       M_CV[CV_idx] <- 0 #Replace approximately 1% of cells in matrix by 0.
       for (s in 1:5){
         NMF <- nmf(M_CV, rank = k, nrun = 10)
-        beta = t(basis(NMF))
         alpha = t(coef(NMF))
         #we replace the zero cells with the estimate from the CV. This is run 5 times, with updated M
         M_CV[CV_idx] <- (t(alpha%*%beta))[CV_idx]
         MSE_CV[s,i] <- mean((as.vector(M[CV_idx])-(t(alpha%*%beta))[CV_idx])^2)
-        print(MSE_CV)
+        #print(MSE_CV)
       }
     }
   MSE[[k]] = MSE_CV
   print(MSE)
   message(100*k/(max(K_range)), "%")
 }
-MSE
+plot(0,0,xlim = c(min(K_range), max(K_range)), ylim = c(0, 60000))
+for(k in K_range){
+  points(rep(k, 10), MSE[[k]][5,], xlim = c(min(K_range), max(K_range)))
+  points(k, mean(MSE[[k]][5,]), col = 2, pch = 16)
+}
