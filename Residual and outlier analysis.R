0689a6a7606b88a446e7796725822d012fb83296
Kim Hochreuter <kho@obh-gruppen.dk>
2021-03-26 13:11:47 +0100

    lol

--- Residual and outlier analysis.R
+++ Residual and outlier analysis.R
@@ -4,11 +4,7 @@
 NMF_final = nmf(M, rank = 6, nrun = 10)
 
 H = basis(NMF_final)
-
 W = coef(NMF_final)
 
 diff = M - H%*%W
-diff
-plot(M[1,],diff[1,])N
-plot(M[3,], diff[3,])
-plot(H%*%W,diff, xlim = c(0,1000),  ylim=c(-500, 500))
+plot(M, diff, xlim = c(0,1000), ylim=c(-500, 500))
