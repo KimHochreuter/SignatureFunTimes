36edf0e73356406ac403a7542e62c07ca757e0a9
Ida Burchardt Egendal <ida@virkplan.dk>
2021-05-26 16:16:06 +0200

    Moved BIC out of CV

--- CV_function_new_Divergence.R
+++ CV_function_new_Divergence.R
@@ -16,7 +16,11 @@ CVNB_D = function(M, K = 10, start_alpha = 10, n_mutatypes = 96,
   
   K_range <- 2:K #Number of signatures to investigate
   
-  D_alpha_CV_NB <- rep(0,7)
+  BIC_matrix <- c(0,0)
+  
+  D_alpha_CV_NB <- rep(0,6)
+  colnames(D_alpha_CV_NB) <- c("K", "alpha", "CV_set", 
+                               "n_update", "D_alpha", "MSE")
   CV_idx <- replicate(n_cv_sets, 
                       list(as.matrix(expand.grid(1:G, 1:m)[sample(1:(G*m), ceiling(G*m*CVentries_percent),replace=FALSE),])))
   
@@ -35,7 +39,7 @@ CVNB_D = function(M, K = 10, start_alpha = 10, n_mutatypes = 96,
                                    # Chose 0.001 because of numeric issues.
       for (s in 1:n_updates) {
         
-        NMF_NB = NMFNBMMsquarem(M_CV_NB, k, alpha = alpha)
+        NMF_NB = NMFNBMMsquarem(M_CV_NB, k, alpha = alpha, arrange = F)
         alpha_NB = t(NMF_NB$E)
         beta_NB = t(NMF_NB$P)
         
@@ -58,16 +62,9 @@ CVNB_D = function(M, K = 10, start_alpha = 10, n_mutatypes = 96,
         
         MSE <-  mean((y-l)^2)
         
-        #number of parameters in the model
-        params <- (dim(alpha_NB)[1]*dim(alpha_NB)[2] + 
-                     dim(beta_NB)[1]*dim(beta_NB)[2] + 1)
-        
-        #number of observations
-        nobs <- ncol(M)*nrow(M)
-        BIC <- 2*log_lik_NB(alpha) + params*log(nobs)
 
         D_alpha_CV_NB <- rbind(D_alpha_CV_NB, c(k, alpha, i, s,
-                                                D_alpha, BIC, MSE))
+                                                D_alpha, MSE))
         
         #update CV entries
         M_CV_NB[CV_idx[[i]]] <- t(alpha_NB%*%beta_NB)[CV_idx[[i]]]
@@ -75,16 +72,36 @@ CVNB_D = function(M, K = 10, start_alpha = 10, n_mutatypes = 96,
       
     }
     
+    g <- as.data.frame(D_alpha_CV_NB)
+    alpha = median(g[g$K == k,]$alpha)
+    
+    NMF_NB = NMFNBMMsquarem(M, k, alpha = alpha, arrange = F)
+    alpha_NB = t(NMF_NB$E)
+    beta_NB = t(NMF_NB$P)
+    
+    #number of parameters in the model
+    params <- (dim(alpha_NB)[1]*dim(alpha_NB)[2] + 
+                 dim(beta_NB)[1]*dim(beta_NB)[2] + 1)
+    
+    #number of observations
+    nobs <- ncol(M)*nrow(M)
+    BIC <- -2*dnbinom(M, alpha, 
+                     1-(alpha_NB%*%beta_NB/(alpha + alpha_NB%*%beta_NB)), 
+                     log = T) + params*log(nobs)
+    
+    BIC_matrix <- rbind(BIC_matrix, c(k, BIC))
+    
     message(round(100*(k-1)/(max(K_range)-1), digits = 2), "%")
   }
   
-  colnames(D_alpha_CV_NB) <- c("K", "alpha", "CV_set", 
-                               "n_update", "D_alpha", "BIC", "MSE")
   D_alpha_CV_NB <- D_alpha_CV_NB[-1,]
+  
+  colnames(BIC_matrix) <- c("K", "BIC")
+  BIC_matrix <- BIC_matrix[-1,]
   
   end_time <- Sys.time()
   print(end_time - start_time)
-  return(D_alpha_CV_NB)
+  return(list(D_alpha_CV_NB, BIC_matrix))
 }
 
 
@@ -100,7 +117,9 @@ CVPO_D = function(M, K = 10, n_mutatypes = 96, n_cv_sets = 10,
   m <- ncol(M) #Number of mutation types
   K_range <- 2:K #Number of signatures to investigate
   
-  DKL_CV_po <- rep(0,6)
+  DKL_CV_po <- rep(0,5)
+  BIC_matrix <- c(0,0)
+  
   CV_idx <- replicate(n_cv_sets, 
                       list(as.matrix(expand.grid(1:G, 1:m)[sample(1:(G*m), ceiling(G*m*CVentries_percent),replace=FALSE),])))
   
@@ -136,29 +155,41 @@ CVPO_D = function(M, K = 10, n_mutatypes = 96, n_cv_sets = 10,
         DKL <- sum(ylogyl(y,l)- y + l)
         MSE <- mean((y-l)^2)
         
-        params <- (dim(alpha_po)[1]*dim(alpha_po)[2] + 
-                     dim(beta_po)[1]*dim(beta_po)[2])
-        nobs <- ncol(M)*nrow(M)
-        BIC <- -2*log_lik_M + params*log(nobs) 
         
-        DKL_CV_po <- rbind(DKL_CV_po,c(k, i, s, DKL, BIC, MSE))
+        DKL_CV_po <- rbind(DKL_CV_po,c(k, i, s, DKL, MSE))
       }
       
     }
     
+    #Run NMF
+    NMF_po <- nmf(M, rank = k, method = "KL")
+    alpha_po = t(coef(NMF_po))
+    beta_po = t(basis(NMF_po))
+    
+    params <- (dim(alpha_po)[1]*dim(alpha_po)[2] + 
+                 dim(beta_po)[1]*dim(beta_po)[2])
+    nobs <- ncol(M)*nrow(M)
+    
+    BIC <- -2*dpois(M, round(alpha_po%*%beta_po), log = T) + params*log(nobs) 
+    
+    BIC_matrix <- rbind(BIC_matrix, c(k, BIC))
+    
     message(round(100*(k-1)/(max(K_range)-1), digits = 2), "%")
   }
   
-  colnames(DKL_CV_po) <- c("K", "CV_set", "n_update", "DKL", "BIC", "MSE")
+  colnames(BIC_matrix) <- c("K", "BIC")
+  BIC_matrix <- BIC_matrix[-1,]
+  
+  colnames(DKL_CV_po) <- c("K", "CV_set", "n_update", "DKL", "MSE")
   DKL_CV_po <- DKL_CV_po[-1,]
   
   end_time <- Sys.time()
   print(end_time - start_time)
-  return(DKL_CV_po)
+  return(list(DKL_CV_po, BIC_matrix))
 }
 
-porund = CVPO_D(V, K = 20)
-nbrund = CVNB_D(V, K = 20)
+porund = CVPO_D(V, K = 15)
+nbrund = CVNB_D(V, K = 15)
 
-porund1_ida = CVPO_D(Liver, K = 20, n_cv_sets = 3)
-nbrund1_ida = CVNB_D(Liver, K = 20, n_cv_sets = 3)
+porund1_ida = CVPO_D(Liver, K = 15)
+nbrund1_ida = CVNB_D(Liver, K = 15)
